5edafecb1441d759c4dee88ac0199e215a09e2fa
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2024-03-24 16:49:13 +0530

    fiftth push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* Accordian component */}
       {/* <Accordian/> */}
       {/* <RandomColor/> */}
-      <StarRating noOfStars={7} />
+      {/* <StarRating noOfStars={7} /> */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"4"} />
     </div>
   );
 }
