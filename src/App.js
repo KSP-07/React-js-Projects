3c1f48370314b8aaa2c5b3992c3e9afc350022ec
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2024-05-17 08:08:54 +0530

    GithubProfileFinder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LightSwitch from "./components/Mode-Switch";
 import Accordian from "./components/accordian";
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import TabTest from "./components/custom-tabs/tab-test";
+import GithubProfileFinder from "./components/github-profile-finder";
 import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import QrCode from "./components/qr-code generator";
@@ -36,7 +37,10 @@ function App() {
       {/* <TabTest/> */}
 
       {/* custom popup */}
-      <ModalTest/>
+      {/* <ModalTest/> */}
+
+      {/* Gtihub data fetch */}
+      <GithubProfileFinder/>
     </div>
   );
 }
